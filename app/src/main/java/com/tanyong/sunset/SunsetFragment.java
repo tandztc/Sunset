22b75c225b877ad725a5d4b2225f9b58c38f879b
tanyong02 <tanyong02@baidu.com>
2016-03-03 11:36:24 +0800

    改变日落时天空颜色。并使用animatorset播放一组动画

--- app/src/main/java/com/tanyong/sunset/SunsetFragment.java
+++ app/src/main/java/com/tanyong/sunset/SunsetFragment.java
@@ -1,12 +1,16 @@
 package com.tanyong.sunset;
 
 
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
 
 
 /**
@@ -19,6 +23,10 @@ public class SunsetFragment extends Fragment {
     private View mSceneView;
     private View mSunView;
     private View mSkyView;
+
+    private int mBlueSkyColor;
+    private int mSunsetSkyColor;
+    private int mNightSkyColor;
 
     public SunsetFragment() {
         // Required empty public constructor
@@ -58,16 +66,36 @@ public class SunsetFragment extends Fragment {
                 startAnimation();
             }
         });
+
+        Resources resources = getResources();
+        mBlueSkyColor = resources.getColor(R.color.blue_sky);
+        mSunsetSkyColor = resources.getColor(R.color.sunset_sky);
+        mNightSkyColor = resources.getColor(R.color.night_sky);
         return view;
     }
 
     private void startAnimation() {
         float sunYStart = mSunView.getTop();
         float sunYEnd = mSkyView.getHeight();
+
         ObjectAnimator heightAnimator = ObjectAnimator
                 .ofFloat(mSunView, "y", sunYStart, sunYEnd)
                 .setDuration(3000);
-        heightAnimator.start();
+        heightAnimator.setInterpolator(new AccelerateInterpolator());
+
+        ObjectAnimator colorAnimator = ObjectAnimator
+                .ofInt(mSkyView, "backgroundColor", mBlueSkyColor, mSunsetSkyColor)
+                .setDuration(3000);
+        colorAnimator.setEvaluator(new ArgbEvaluator());
+
+        ObjectAnimator nightColorAnimator = ObjectAnimator.
+                ofInt(mSkyView, "backgroundColor", mSunsetSkyColor, mNightSkyColor)
+                .setDuration(1000);
+        nightColorAnimator.setEvaluator(new ArgbEvaluator());
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.play(heightAnimator).with(colorAnimator).before(nightColorAnimator);
+        animatorSet.start();
     }
 
 }
