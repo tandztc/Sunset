a5250a1b99dfcbc11f01daf334233bb9c83df20e
tanyong02 <tanyong02@baidu.com>
2016-03-03 10:46:07 +0800

    创建工程

---
+++ app/src/main/java/com/tanyong/sunset/SunsetActivity.java
@@ -0,0 +1,11 @@
+package com.tanyong.sunset;
+
+import android.support.v4.app.Fragment;
+
+public class SunsetActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return SunsetFragment.newInstance();
+    }
+}
---
+++ app/src/main/java/com/tanyong/sunset/SunsetFragment.java
@@ -0,0 +1,49 @@
+package com.tanyong.sunset;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SunsetFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SunsetFragment extends Fragment {
+
+    public SunsetFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment SunsetFragment.
+     */
+    public static SunsetFragment newInstance() {
+        SunsetFragment fragment = new SunsetFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sunset, container, false);
+    }
+
+}
